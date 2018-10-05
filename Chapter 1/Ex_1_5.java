e064a9c10e59a236255a94663d016e9f39f4060f
Stetson <stets.serna@gmail.com>
2018-10-05 09:39:54 -0600

    Added Chapter 1

---
+++ Chapter 1/Ex_1_5.java
@@ -0,0 +1,13 @@
+/*
+Author: Stetson Serna
+Date: 9/19/2018
+
+Compute expression
+*/
+
+
+class Ex_1_5  {
+	public static void main(String[] args) {
+		System.out.println(((9.5*4.5)-(2.5*3))/(45.5-3.5));
+	}
+}
---
+++ Chapter 1/Ex_1_9.java
@@ -0,0 +1,13 @@
+/*
+Author:Stetson Serna
+Date: 9/25/2018
+
+Compute expression
+*/
+
+class Ex_1_9 {
+	public static void main(String[] args) {
+	System.out.println(4.5*7.9);
+	System.out.println((4.5*2)+(7.9*2));	
+	}
+}
