ffe2b91f81827c2910fd9255bd40f64f8132417a
StetsonS <stets.serna@gmail.com>
2018-10-11 17:29:47 -0600

    Edited Chapter 2

--- Chapter 2/Ex_2_13.java
+++ Chapter 2/Ex_2_13.java
@@ -1,5 +1,32 @@
-class Ex_2_13 {
+/*
+  Author Stetson Serna
+  Date 10/11/18
+ */
+import java.util.Scanner;
+
+	class Ex_2_13 {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);	// Create a new Scanner object.
+		final double MONTHLY_INTEREST_RATE = 0.00417;	// Initialize constant value
+
+		// user enters monthly value
+		System.out.print("Enter the monthly saving amount: ");
+		double savingAmount = input.nextDouble();
+
+		// First months value
+		double total = 100 * (1 + MONTHLY_INTEREST_RATE);
+		// second months value
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		// third months value
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		// forth months value
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		// fifth months  value
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+		// sixth months  value
+		total = (100 + total) * (1 + MONTHLY_INTEREST_RATE);
+
+		// Display result
+		System.out.println("After the sixth month, the account value is " + total);
 	}
 }
--- Chapter 2/Ex_2_23.java
+++ Chapter 2/Ex_2_23.java
@@ -1,5 +1,27 @@
-class Ex_2_23 {
+/*Author Stetson Serna
+ * Date 10/11/2018
+ */
+import java.util.Scanner;
+	class Ex_2_23 {
 	public static void main(String[] args) {
-		
+	Scanner input = new Scanner(System.in);//New Scanner
+	
+	//User enters distance
+	System.out.print("Enter the Driving Distance");
+	double distance=input.nextDouble();
+	
+	//User enters Miles per Gallon
+	System.out.print("Enter miles per gallon");
+	double miles per gallon=input.nextDouble();
+			
+	//User enter price per gallon
+	System.out.print("Enter price per gallon");
+	double price per gallon=input.nextDouble();
+	
+	// Compute the cost of driving
+			double cost of driving = (distance / miles per gallon) * price per gallon;
+
+			// Display result
+			System.out.println("The cost of driving is $" + cost of driving);
 	}
 }
