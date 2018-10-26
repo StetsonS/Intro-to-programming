36b2b9ff8d588bf6006ec7fc59abcc0cecb31656
Stetson <stets.serna@gmail.com>
2018-10-26 10:04:26 -0600

    Yea

---
+++ Chapter 4/Excercise4_17.java
@@ -0,0 +1,26 @@
+/*
+Author: Stetson Serna
+Date: 10/26/2018
+*/
+
+import java.util.Scanner;
+
+class Exercise4_17 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Enter a year: ");
+		int year = input.nextInt();
+		System.out.print(" Enter a month, only 3 letters please uppercase the first letter ");
+		String month = input.next();
+		
+		if (month is Jan or month is Mar or month is May){
+			System.out.print(" That month has 31 days ");
+		}
+		else if (month is Apr or month is )
+			System.out.print(" that month has 30 days ");
+			
+		else if (month is Fev) {
+			if 
+		}
+	}
+}
