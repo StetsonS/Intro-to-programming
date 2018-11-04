10428a40799363be0772d2d9b428efee8fa2f6fd
StetsonS <stets.serna@gmail.com>
2018-11-04 10:20:42 -0700

    Updated 4_17

--- Chapter 4/Excercise4_17.java
+++ Chapter 4/Excercise4_17.java
@@ -13,8 +13,22 @@ class Exercise4_17 {
 		System.out.print(" Enter a month, only 3 letters please uppercase the first letter ");
 		String month = input.next();
 		
-		if( month () = Jan ){
-		System.out.println( " month has 31 Days");
+		if ((month.equalsIgnoreCase("Jan")) || (month.equalsIgnoreCase("Mar")) || (month.equalsIgnoreCase("May"))  || month.equalsIgnoreCase("Dec")) {
+			System.out.print("That month has 31 days");
+		} else if ((month.equalsIgnoreCase("Apr") || month.equalsIgnoreCase("Jun") || month.equalsIgnoreCase("Sep")) || month.equalsIgnoreCase("Nov")){
+			System.out.print("That month has 30 days");
+		} else if (month.equalsIgnoreCase("Feb")) {
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+				System.out.print("That month has 29 days");
+			}
+			else {
+				System.out.print("That month has 28 days");
+			}
+			//print out 28 on a non leap year
+			//or print out 29 on a leap yar
+		}
+		else {
+			System.out.print("That is not a real month, try typing only 3 letters");
 		}
 	}
 }
