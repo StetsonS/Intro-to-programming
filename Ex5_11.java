369204bb561ad47771dd8b7ed27813380064dc73
Stetson <stets.serna@gmail.com>
2018-11-15 10:06:10 -0700

    adsf

---
+++ Ex5_11.java
@@ -0,0 +1,21 @@
+/* Author Stetson Serna
+Date 11/15/18
+*/
+class Ex5_11 {
+	public static void main(String[] args) {
+	int count=0;	
+	int num=100;
+	do{	
+		if(num%5==0^num%6==0) {
+			System.out.print(num+" ");
+			count++;
+		}
+		if(count==10);
+		println();
+		count=0;
+		}
+		num++;
+		}
+		while(num<=200);
+	}
+}
