fd727db60f2287cd913a1da058f3b29219daff9b
StetsonS <stets.serna@gmail.com>
2018-12-08 17:33:44 -0700

    fixed

--- Ex_6_3.java
+++ Ex_6_3.java
@@ -1,32 +1,43 @@
+/*
+ * Author: Stetson Serna
+ * Date: 12/8/2018
+ */
+
+import java.util.Scanner;
+
 class Ex_6_3 {
 	public static int reverse(int number) {
-		int user = 0;
-		int num = 0;
+		
+		
 		int remain = 0;
 		int reverse = 0;
 	
 
-		
-		num = user;
-			while (num != 0) {
-				remain = num % 10;
+			while (number != 0) {
+				remain = number % 10;
 				reverse = reverse * 10 + remain;
-				num/=10;
+				number = number/=10;
 			}
-
-	public static boolean 1stPalindome(int number) {
+			return reverse;
+	}
+	public static boolean isPalindrome(int number) {
 		boolean isPalindrome = false;
 			//call reverse method and check if palindrome
-			
+			int reverse = reverse (number);
+			if (reverse == number) {
+				isPalindrome = true;
+			}
+		
+	
 			return isPalindrome;
-
+	}
 		public static void main(String [] args) {
 			// Declare variables
 			Scanner input = new Scanner(System.in);
 			int user = 0;
 			
 			//prompt user
-			System.out.print(" Enter a month ");
+			System.out.print(" Enter a number ");
 			user = input.nextInt();
 			System.out.println("Is " + user + " a palindrome? " + isPalindrome(user));
 		
