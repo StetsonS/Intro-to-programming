3ae9b16ee6c26617366df2a8f70816d4cbe60ac3
StetsonS <stets.serna@gmail.com>
2018-12-18 18:53:46 -0700

    me llamo greg

--- Ex_6_17.java
+++ Ex_6_17.java
@@ -3,13 +3,22 @@ Author Stetson Serna
 Date 12/18/18
 */
 
-
+import java.util.Scanner;
 class Ex_6_17 {
 	public static void main(String[] args) {
-		for (rows)&
-			for(columns)&
-				(int)(min+Math.random() max+1)
-					(int)(20+Math.random(1)51)
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println(" Type in a Number Please: ");
+			int n =input.nextInt();
+			printMatrix(n);
+			
+		}
+		public static void printMatrix(int n) {
+			for (int x = 0; x < n; x++) {
+			for (int y = 0; x < n; y++) {
+				System.out.print((int) (Math.random() * 2)+ " ");
+		}
+		System.out.println();
+
+		}
 	}
 }
