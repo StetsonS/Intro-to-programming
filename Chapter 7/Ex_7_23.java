7a17774a26b0765378dcc25c3142a5adfaccef48
Stetson <stets.serna@gmail.com>
2019-02-15 09:45:05 -0700

    added chapter 7

---
+++ Chapter 7/Ex_7_23.java
@@ -0,0 +1,16 @@
+/*
+Author: Stetson Serna
+Date: 2/15/19
+*/
+
+class Ex_7_23 {
+	public static void main(String[] args) {
+		
+	}
+}
+boolean [] locker = new boolean [100];
+for(int < -1 ; <=100; s++){
+	for(int k = (s - 1); k <100; k + -5){
+		
+	}
+}
